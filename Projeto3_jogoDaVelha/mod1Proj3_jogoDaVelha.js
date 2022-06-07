3141a5f444ff61d928ee0a0b3d1312dfb1577ce1
Vanderlei Miguel <vanderleimiguel@hotmail.com>
2022-06-07 16:19:49 -0300

    funcionando, falta acertar empate

--- Projeto3_jogoDaVelha/mod1Proj3_jogoDaVelha.js
+++ Projeto3_jogoDaVelha/mod1Proj3_jogoDaVelha.js
@@ -25,6 +25,9 @@ let symbolUser = 0
 let symbolPC = 0
 let endGame = 'N'
 let positionFree = 'N'
+let winnerUser = 0
+let winnerPC = 0
+let continuar = 'S'
 
 //******************************************************************
 //Interação com usuario, selecionar simbolo, linha e coluna
@@ -35,10 +38,10 @@ console.log('Vamos jogar o classico Jogo da Velha!!!!')
 console.log('Vamos começar a jogar!!!!!!')
 
 //escolha x ou 0
-symbol = +prompt('\nEscolha seu símbolo para jogar [1]X ou [2]O: ')
+symbol = +prompt('\nEscolha seu símbolo para jogar [1] X ou [2] O: ')
 while (symbol != 1 && symbol != 2) {
   console.log('\nDigite apenas 1 ou 2')
-  symbol = +prompt('Escolha seu símbolo para jogar [1]X ou [2]O: ')
+  symbol = +prompt('Escolha seu símbolo para jogar [1] X ou [2] O: ')
 }
 if (symbol == 1) {
   symbolUser = 'X'
@@ -52,71 +55,103 @@ if (symbol == 1) {
 console.log(tabuleiro)
 
 //Escolhas de posição
-while (endGame == 'N') {
-  while (positionFree == 'N') {
-    //Usuario escolha a linha
-    lineUser = +prompt(
-      `\nEscolha a linha [1], [2] ou [3] que deseja colocar o ${symbolUser}: `
-    )
-    while (lineUser != 1 && lineUser != 2 && lineUser != 3) {
-      console.log('\nDigite apenas [1], [2] ou [3]')
+while (continuar == 'S') {
+  while (endGame == 'N') {
+    while (positionFree == 'N') {
+      //Usuario escolha a linha
       lineUser = +prompt(
-        `Escolha a linha [1], [2] ou [3] que deseja colocar o ${symbolUser}: `
+        `\nEscolha a linha [1], [2] ou [3] que deseja colocar o ${symbolUser}: `
       )
-    }
-    //Usuario escolha coluna
-    columUser = prompt(
-      `Escolha a coluna [A], [B] ou [C] que deseja colocar o ${symbolUser}: `
-    ).toUpperCase()
-    while (columUser != 'A' && columUser != 'B' && columUser != 'C') {
-      console.log()
-      console.log('\nDigite apenas [A], [B] ou [C]')
+      while (lineUser != 1 && lineUser != 2 && lineUser != 3) {
+        console.log('\nDigite apenas [1], [2] ou [3]')
+        lineUser = +prompt(
+          `Escolha a linha [1], [2] ou [3] que deseja colocar o ${symbolUser}: `
+        )
+      }
+      //Usuario escolha coluna
       columUser = prompt(
         `Escolha a coluna [A], [B] ou [C] que deseja colocar o ${symbolUser}: `
       ).toUpperCase()
-      console.log()
-    }
+      while (columUser != 'A' && columUser != 'B' && columUser != 'C') {
+        console.log()
+        console.log('\nDigite apenas [A], [B] ou [C]')
+        columUser = prompt(
+          `Escolha a coluna [A], [B] ou [C] que deseja colocar o ${symbolUser}: `
+        ).toUpperCase()
+        console.log()
+      }
 
-    //Verifica posicao livre
-    positionFree = position(lineUser, columUser)
-    if (positionFree == 'N') {
-      console.log(`\nPosição ${lineUser}, ${columUser} está ocupada`)
-    }
-  }
-  positionFree = 'N'
-  //coloca escolha no tabuleiro
-  choice(symbolUser, lineUser, columUser)
-
-  //verifica se conclui o jogo
-  endGame = winner()
-
-  if ((endGame = 'N')) {
-    while (positionFree == 'N') {
-      columPC = LetterPC[Math.floor(Math.random() * 3)]
-      linePC = Math.floor(Math.random() * 3 + 1)
-      positionFree = position(linePC, columPC)
+      //Verifica posicao livre
+      positionFree = position(lineUser, columUser)
+      if (positionFree == 'N') {
+        console.log(`\nPosição ${lineUser}, ${columUser} está ocupada`)
+      }
     }
     positionFree = 'N'
     //coloca escolha no tabuleiro
-    choice(symbolPC, linePC, columPC)
+    choice(symbolUser, lineUser, columUser)
 
     //verifica se conclui o jogo
     endGame = winner()
-  }
-  console.log(tabuleiro)
-}
+    console.log(`user ${endGame}`)
 
-//Mostra na tela quem foi o ganhador
-if (
-  (endGame == 'O' && symbolUser == 'O') ||
-  (endGame == 'X' && symbolUser == 'X')
-) {
-  console.log('\nParabens você foi o ganhador!!!!!!!')
-} else if (
-  (endGame == 'O' && symbolPC == 'O') ||
-  (endGame == 'X' && symbolPC == 'X')
-) {
-  console.log('\nVocê perdeu!!!!!!')
+    if ((endGame = 'N')) {
+      while (positionFree == 'N') {
+        columPC = LetterPC[Math.floor(Math.random() * 3)]
+        linePC = Math.floor(Math.random() * 3 + 1)
+        positionFree = position(linePC, columPC)
+      }
+      positionFree = 'N'
+      //coloca escolha no tabuleiro
+      choice(symbolPC, linePC, columPC)
+
+      //verifica se conclui o jogo
+      endGame = winner()
+      console.log(`PC ${endGame}`)
+    }
+
+    console.log(tabuleiro)
+  }
+
+  //Mostra na tela quem foi o ganhador
+  if (
+    (endGame == 'O' && symbolUser == 'O') ||
+    (endGame == 'X' && symbolUser == 'X')
+  ) {
+    winnerUser++
+    console.log('\nParabens você foi o ganhador!!!!!!!')
+    console.log(
+      `Você ganhou ${winnerUser} vezes e o PC ganhou ${winnerPC} vezes`
+    )
+  } else if (
+    (endGame == 'O' && symbolPC == 'O') ||
+    (endGame == 'X' && symbolPC == 'X')
+  ) {
+    winnerPC++
+    console.log('\nVocê perdeu!!!!!!')
+    console.log(
+      `Você ganhou ${winnerUser} vezes e o PC ganhou ${winnerPC} vezes`
+    )
+  } else if (endGame == 'E') {
+    console.log('\nJogo empatado!!!!!!')
+    console.log(
+      `Você ganhou ${winnerUser} vezes e o PC ganhou ${winnerPC} vezes`
+    )
+  }
+
+  //zerando variaveis
+  endGame = 'N'
+  tabuleiro = [
+    [0, 'A', 'B', 'C'],
+    [1, '_', '_', '_'],
+    [2, '_', '_', '_'],
+    [3, '_', '_', '_']
+  ]
+
+  continuar = prompt('Deseja continuar [S/N]').toUpperCase()
+  while (continuar != 'S' && continuar != 'N') {
+    continuar = prompt('Deseja continuar [S/N]').toUpperCase()
+  }
 }
 
 //******************************************************************
@@ -148,103 +183,103 @@ function choice(symbol, line, column) {
 
 //função verifica se houve ganhador
 function winner() {
-  let venceu = 'N'
+  let venceu = 'Y'
   if (
     tabuleiro[1][1] == 'O' &&
     tabuleiro[1][2] == 'O' &&
     tabuleiro[1][3] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[2][1] == 'O' &&
     tabuleiro[2][2] == 'O' &&
     tabuleiro[2][3] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[3][1] == 'O' &&
     tabuleiro[3][2] == 'O' &&
     tabuleiro[3][3] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[1][1] == 'O' &&
     tabuleiro[2][2] == 'O' &&
     tabuleiro[3][3] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[1][3] == 'O' &&
     tabuleiro[2][2] == 'O' &&
     tabuleiro[3][1] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[1][1] == 'O' &&
     tabuleiro[2][1] == 'O' &&
     tabuleiro[3][1] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[1][2] == 'O' &&
     tabuleiro[2][2] == 'O' &&
     tabuleiro[3][2] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[1][3] == 'O' &&
     tabuleiro[2][3] == 'O' &&
     tabuleiro[3][3] == 'O'
   ) {
-    venceu = '0'
+    venceu = 0
   } else if (
     tabuleiro[1][1] == 'X' &&
     tabuleiro[1][2] == 'X' &&
     tabuleiro[1][3] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[2][1] == 'X' &&
     tabuleiro[2][2] == 'X' &&
     tabuleiro[2][3] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[3][1] == 'X' &&
     tabuleiro[3][2] == 'X' &&
     tabuleiro[3][3] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[1][1] == 'X' &&
     tabuleiro[2][2] == 'X' &&
     tabuleiro[3][3] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[1][3] == 'X' &&
     tabuleiro[2][2] == 'X' &&
     tabuleiro[3][1] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[1][1] == 'X' &&
     tabuleiro[2][1] == 'X' &&
     tabuleiro[3][1] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[1][2] == 'X' &&
     tabuleiro[2][2] == 'X' &&
     tabuleiro[3][2] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   } else if (
     tabuleiro[1][3] == 'X' &&
     tabuleiro[2][3] == 'X' &&
     tabuleiro[3][3] == 'X'
   ) {
-    venceu = '1'
+    venceu = 1
   }
   if (venceu == 0) {
     return 'O'
