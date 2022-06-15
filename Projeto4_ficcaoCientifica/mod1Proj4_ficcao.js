4e3089221bf8d38c1b1ee166e4dade686a08181a
Vanderlei Miguel <vanderleimiguel@hotmail.com>
2022-06-14 23:08:03 -0300

    2022_06_14_estruturaInicial

--- Projeto4_ficcaoCientifica/mod1Proj4_ficcao.js
+++ Projeto4_ficcaoCientifica/mod1Proj4_ficcao.js
@@ -10,11 +10,10 @@ console.clear()
 //**************************************************************
 
 let personagem = ''
-let qtdInimigos = 0
 let escolha = 0
 let retorno = ''
-let cicloJogo = true
 let vida = true
+let escolhaEnergia = 0
 let escolhaTreinamento = 0
 let escolhaAleatoria = 0
 let num = 0
@@ -22,14 +21,15 @@ let num = 0
 let dados = {
   dia: 1,
   forca: 1,
-  energia: 5
+  energia: 1
 }
 
-let escolha1 = [
-  `[1] Treinar`,
+let escolhaPrincipal = [
+  `\n[1] Treinar`,
   `\n[2] Alimentar`,
-  `\n[3] Fazer ronda`,
-  `\n[4] Desafiar Samurai Supremo`
+  '\n[3] Desafiar mestre',
+  `\n[4] Desafiar lider Supremo`,
+  '\n[5] Desistir'
 ]
 
 //******************************************************************
@@ -42,13 +42,18 @@ function aleatorio() {
   return num
 }
 
+//Função Continuar historia
+function continuar() {
+  console.log()
+  prompt(`Pressione ENTER para continuar...`)
+}
+
 //Função Treinamento
 function treinamento() {
-  console.log(dados.energia)
   escolhaTreinamento =
     +prompt(`\n${personagem} Qual tipo de treinamento deseja realizar: 
     [1] - Treinamento com boneco.
-    [2] - Treinamento de Luta .
+    [2] - Treinamento com companheiro de Luta.
     [3] - Treinamento de caça de animais.
     `)
   if (escolhaTreinamento == 1) {
@@ -58,9 +63,7 @@ function treinamento() {
     dados.energia - 2
     dados.forca + 3
   } else if (escolhaTreinamento == 3) {
-    console.log('entrou')
     escolhaAleatoria = aleatorio()
-    console.log(`${escolhaAleatoria} numero`)
     if (escolhaAleatoria == 1) {
       console.log(
         'Sua caçada foi um sucesso, você acertou um alce em apenas um tiro, nem deu para se cançar.'
@@ -82,26 +85,49 @@ function treinamento() {
   }
 }
 
-//Função alimentação
-function alimentacao(escolhaAlimentar) {
-  dados.energia++
-  return 1
+//Função ganhar energia
+function ganharEnergia() {
+  escolhaEnergia = +prompt(`\n${personagem} para se alimentar temos 3 opções: 
+  [1] - Podemos dividir o que temos disponivel.
+  [2] - Colher o que plantamos.
+  [3] - Tentar caçar algo para comer.
+  `)
+  if (escolhaEnergia == 1) {
+    dados.energia--
+    dados.forca++
+  } else if (escolhaEnergia == 2) {
+    dados.energia - 2
+    dados.forca + 3
+  } else if (escolhaEnergia == 3) {
+    escolhaAleatoria = aleatorio()
+    if (escolhaAleatoria == 1) {
+      console.log(
+        'Sua caçada foi um sucesso, você acertou um alce em apenas um tiro, nem deu para se cançar.'
+      )
+      dados.forca += 2
+    } else if (escolhaAleatoria == 2) {
+      console.log(
+        'Você encontrou um urso feroz no seu caminho e teve muito trabalho para derrota-lo, conseguiu aumentar sua força, mais lhe custou muita energia'
+      )
+      dados.energia -= 2
+      dados.forca += 3
+    } else if (escolhaAleatoria == 3) {
+      console.log(
+        'Você encontrou uma grande patrulha inimiga em seu treinamento e não foi capaz de derrota-los, onde acabou morto em batalha como um verdadeiro guerreiro'
+      )
+      vida = false
+    }
+  }
 }
 
-//Função Ronda
-function ronda() {}
+//Função Desafiar Mestre
+function desafiarMestre() {}
 
-//Função Desafiar o lider
-function desafiar() {}
-
-//Função Continuar historia
-function continuar() {
-  console.log()
-  prompt(`Pressione ENTER para continuar...`)
-}
+//Função Desafiar o lider Supremo
+function desafiarLiderSupremo() {}
 
 //******************************************************************
-//Escolhas
+//Interação com usuario, historia
 //******************************************************************
 
 //Introdução
@@ -117,28 +143,29 @@ console.clear()
 console.log()
 
 //nome de seu personagem
-personagem = prompt('\nPescador: Como você se chama?')
+personagem = prompt('Qual o nome de seu personagem?')
 console.log()
 
 console.log(
   `${personagem} Venha a minha casa, posso lhe oferecer comida em troca de segurança da vila enquanto se recupera totalmente, você esta com fome e fraco, para reconquistar o castelo sugiro que você se alimente e treine primeiramente`
 )
-dados.energia++
-console.log(`${personagem} recebeu +1 de alimento`)
+
 continuar()
 console.clear()
 
 while (vida === true) {
+  //dados atualizados
   console.log(`Você esta no dia ${dados.dia},
-  com ${dados.energia} de alimento e
+  com ${dados.energia} de energia e
   ${dados.forca} de força`)
   console.log()
 
-  console.log('Escolha o que deseja fazer no momento: ')
-  console.log((escolha = +prompt(`${escolha1}`)))
+  //Escolha principal da atividade a ser realizada
+  escolha = +prompt(
+    `Escolha o que deseja fazer no momento: ${escolhaPrincipal}`
+  )
 
   //Resultado das escolhas
-
   if (escolha == 1) {
     console.log(`${personagem}: escolheu treinar`)
     if (dados.energia <= 2) {
@@ -150,19 +177,14 @@ while (vida === true) {
     }
   } else if (escolha == 2) {
     console.log(`${personagem}: escolheu se alimentar`)
-    escolhaAlimentar =
-      +prompt(`\n${personagem} para se alimentar temos 3 opções: 
-    [1] - Podemos dividir o que temos disponivel.
-    [2] - Colher o que plantamos.
-    [3] - Tentar caçar algo assim já servindo de treinamento também.
-    `)
-    retorno = alimentar(escolhaAlimentar)
+    retorno = ganharEnergia()
   } else if (escolha == 3) {
     console.log(`${personagem}: escolheu se fazer a ronda`)
-    retorno = ronda()
-    dados.energia--
-  } else {
-    console.log(`${personagem}: escolheu desafiar o lider ninja`)
-    retorno = desafiar()
+    retorno = desafiarMestre()
+  } else if (escolha == 4) {
+    console.log(`${personagem}: escolheu se fazer a ronda`)
+    retorno = desafiarLiderSupremo()
+  } else if (escolha == 5) {
+    console.log(`${personagem}: escolheu se fazer a ronda`)
   }
 }
