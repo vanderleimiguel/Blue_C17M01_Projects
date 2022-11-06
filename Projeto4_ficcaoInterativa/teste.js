1ac1788d7a18dd4c13481a40774c3a15573f34d2
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-06 07:55:04 -0300

    teste

---
+++ Projeto4_ficcaoInterativa/teste.js
@@ -0,0 +1,29 @@
+const main = params => {
+  let list = []
+  numbers(1000)
+
+  function numbers(num) {
+    for (let i = 2; i <= num; i++) {
+      if (numbersPrimo(i)) {
+        list.push(i)
+      }
+    }
+  }
+
+  function numbersPrimo(num2) {
+    for (let index = 2; index < num2; index++) {
+      if (num2 % index === 0) {
+        return false
+      }
+    }
+    return true
+  }
+
+  var sum = 0
+  for (var i = 0; i < list.length; i++) {
+    sum += list[i]
+  }
+
+  return sum
+}
+main()
